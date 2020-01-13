5556031178b48b7c13d6829089197f85b1a4e6e1
Suleyman63701 <suleymanslk@gmail.com>
2020-01-13 16:35:15 -0600

    test

---
+++ app.js
@@ -0,0 +1,22 @@
+let message=document.getElementById('message');
+
+ window.onload=init;
+
+function init(){
+  navigator.geolocation.getCurrentPosition(locationData)
+    
+
+}
+
+function locationData(data){
+//console.log(data)
+message.innerHTML +=`Latitude: ${data.coords.latitude}<br>`
+message.innerHTML +=`Longitude: ${data.coords.longitude}`
+let map;
+map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: data.coords.latitude, lng: data.coords.longitude},
+    zoom: 8
+})
+}
+
+//AIzaSyDlwGSvYwCOHzFBd8Ha-LxisZskz1oeoac
